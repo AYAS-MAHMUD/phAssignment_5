6029598608b5e4fd1669b6dda27d22413ac5e736
Ayas Mahmud <ayasmahmud48@gmail.com>
2025-08-28 22:22:58 +0600

    Added copy functionality

--- script.js
+++ script.js
@@ -50,6 +50,12 @@ clickCopy.forEach((btn, i) => {
     btn.addEventListener('click', () => {
         alert("Number Copied : " + callNumber[i].innerText);
         copycount.innerText++;
+        var text = "" + callNumber[i].innerText;
+        navigator.clipboard.writeText(text).then(function() {
+        console.log('Async: Copying to clipboard was successful!');
+        }, function(err) {
+        console.error('Async: Could not copy text: ', err);
+        });
     });
 });
 
